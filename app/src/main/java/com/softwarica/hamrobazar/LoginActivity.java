96fc22aa9191ef614e04a35d85fdeff9848c2db5
bipul <bipulkansakar@gmail.com>
2020-01-08 16:06:43 +0545

    Login Activity

--- app/src/main/java/com/softwarica/hamrobazar/LoginActivity.java
+++ app/src/main/java/com/softwarica/hamrobazar/LoginActivity.java
@@ -2,13 +2,64 @@ package com.softwarica.hamrobazar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.softwarica.hamrobazar.R;
+import com.softwarica.hamrobazar.bll.LoginBLL;
+import com.softwarica.hamrobazar.strictmode.StrictModeClass;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etEmail, etPassword;
+    private Button btnLogin;
+    private Button btnRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        String username = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        LoginBLL loginBLL = new LoginBLL();
+
+        StrictModeClass.StrictMode();
+        if (loginBLL.checkUser(username, password)) {
+            Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(this, "Either email or password is incorrect", Toast.LENGTH_SHORT).show();
+            etEmail.requestFocus();
+        }
+
     }
 }
