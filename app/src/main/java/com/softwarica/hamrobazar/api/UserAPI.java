16e29bf7e17e1f03143df618e3a1193d62386477
bipul <bipulkansakar@gmail.com>
2020-01-08 16:54:23 +0545

    User API Login BLL

---
+++ app/src/main/java/com/softwarica/hamrobazar/api/UserAPI.java
@@ -0,0 +1,29 @@
+package com.softwarica.hamrobazar.api;
+
+import com.softwarica.hamrobazar.model.User;
+import com.softwarica.hamrobazar.serverresponse.ImageResponse;
+import com.softwarica.hamrobazar.serverresponse.SignUpResponse;
+
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface UsersAPI {
+
+    @POST("users/signup")
+    Call<SignUpResponse> registerUser(@Body User users);
+
+    @FormUrlEncoded
+    @POST("users/login")
+    Call<SignUpResponse> checkUser(@Field("username") String username, @Field("password") String password);
+
+    @Multipart
+    @POST("upload")
+    Call<ImageResponse> uploadImage(@Part MultipartBody.Part img);
+
+}
