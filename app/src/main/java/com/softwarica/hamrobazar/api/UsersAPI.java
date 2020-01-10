0d0bfaa8950e2115d6673b2b6e37f4f1ed025a64
bipul <bipulkansakar@gmail.com>
2020-01-10 08:50:22 +0545

    API added

--- app/src/main/java/com/softwarica/hamrobazar/api/UsersAPI.java
+++ app/src/main/java/com/softwarica/hamrobazar/api/UsersAPI.java
@@ -3,15 +3,19 @@ package com.softwarica.hamrobazar.api;
 import android.media.Image;
 import android.os.StrictMode;
 
+import com.softwarica.hamrobazar.model.Product;
 import com.softwarica.hamrobazar.model.User;
 import com.softwarica.hamrobazar.serverresponse.ImageResponse;
 import com.softwarica.hamrobazar.serverresponse.SignUpResponse;
+
+import java.util.List;
 
 import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
@@ -28,4 +32,7 @@ public interface UsersAPI {
     @Multipart
     @POST("upload")
     Call<ImageResponse> uploadImage(@Part MultipartBody.Part img);
+
+    @GET("product")
+    Call<List<Product>>getAllProducts();
 }
