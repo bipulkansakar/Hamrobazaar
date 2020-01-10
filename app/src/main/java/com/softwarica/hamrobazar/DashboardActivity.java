b525c218515c43ca1fd488e35e4ce698f018f99f
bipul <bipulkansakar@gmail.com>
2020-01-10 17:05:46 +0545

    DashboardActivity Commit

--- app/src/main/java/com/softwarica/hamrobazar/DashboardActivity.java
+++ app/src/main/java/com/softwarica/hamrobazar/DashboardActivity.java
@@ -2,17 +2,38 @@ package com.softwarica.hamrobazar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.softwarica.hamrobazar.LoginActivity;
 import com.softwarica.hamrobazar.R;
+import com.softwarica.hamrobazar.adapter.ProductsAdapter;
 import com.softwarica.hamrobazar.adapter.imgSliderAdapter;
+import com.softwarica.hamrobazar.api.UsersAPI;
+import com.softwarica.hamrobazar.model.Product;
+import com.softwarica.hamrobazar.model.User;
+import com.softwarica.hamrobazar.url.Url;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -21,9 +42,13 @@ public class DashboardActivity extends AppCompatActivity {
     private int dotscount;
     private ImageView[] dots;
 
-    private ImageView imgLogin;
+    private CircleImageView imgLogin;
 
-    public DashboardActivity(){}
+    private RecyclerView rvProduct;
+    SwipeRefreshLayout refreshLayout;
+
+    public DashboardActivity() {
+    }
 
 
     @Override
@@ -31,15 +56,23 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        rvProduct = findViewById(R.id.rvProduct);
+        refreshLayout = findViewById(R.id.refreshLayout);
+        showProduct();
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                showProduct();
+
+            }
+
+        });
+
+        loadCurrentUser();
+
+
         viewPager = findViewById(R.id.imgSlider);
         imgLogin = findViewById(R.id.imgLogin);
-
-        imgLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(DashboardActivity.this, LoginActivity.class));
-            }
-        });
 
         //For indicators
         sliderDotsPanel = findViewById(R.id.sliderDotsPanel);
@@ -98,12 +131,11 @@ public class DashboardActivity extends AppCompatActivity {
         @Override
         public void run() {
 
-            if (!pagerMoved){
-                if (viewPager.getCurrentItem() == viewPager.getChildCount()){
-                    viewPager.setCurrentItem(0,true);
-                }
-                else {
-                    viewPager.setCurrentItem(viewPager.getCurrentItem()+1, true);
+            if (!pagerMoved) {
+                if (viewPager.getCurrentItem() == viewPager.getChildCount()) {
+                    viewPager.setCurrentItem(0, true);
+                } else {
+                    viewPager.setCurrentItem(viewPager.getCurrentItem() + 1, true);
                 }
                 pagerMoved = false;
                 h.postDelayed(animateViewPager, ANIM_VIEWPAGER_DELAY);
@@ -115,7 +147,7 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
 
-        if (h != null){
+        if (h != null) {
             h.removeCallbacks(animateViewPager);
         }
     }
@@ -125,8 +157,90 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        h.postDelayed(animateViewPager,ANIM_VIEWPAGER_DELAY);
+        h.postDelayed(animateViewPager, ANIM_VIEWPAGER_DELAY);
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+
+    private void showProduct() {
+        refreshLayout.setRefreshing(false);
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(Url.base_url).addConverterFactory(GsonConverterFactory.create()).build();
+        UsersAPI usersAPI = retrofit.create(UsersAPI.class);
+
+        Call<List<Product>> proListCall = usersAPI.getAllProducts();
+
+        proListCall.enqueue(new Callback<List<Product>>() {
+            @Override
+            public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
+                if (!response.isSuccessful()) {
+                    refreshLayout.setRefreshing(true);
+                    Toast.makeText(DashboardActivity.this, "Error code " + response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                List<Product> ProductList = response.body();
+                ProductsAdapter productsAdapter = new ProductsAdapter(DashboardActivity.this, ProductList);
+                rvProduct.setAdapter(productsAdapter);
+                rvProduct.setLayoutManager(new LinearLayoutManager(DashboardActivity.this, LinearLayoutManager.HORIZONTAL, false));
+
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Product>> call, Throwable t) {
+
+                Log.d("Msg", "onFailure" + t.getLocalizedMessage());
+                Toast.makeText(DashboardActivity.this, "Error" + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void loadCurrentUser () {
+        UsersAPI usersAPI = Url.getInstance().create(UsersAPI.class);
+        Call<User> userCall = usersAPI.getUserDetails(Url.token);
+        userCall.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, final Response<User> response) {
+                if (!response.isSuccessful()) {
+//                    Toast.makeText(DashboardActivity.this, "Code "+response.code(), Toast.LENGTH_SHORT).show();
+
+                    //to show login and registration when user not logged in
+
+                    imgLogin.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            startActivity(new Intent(DashboardActivity.this,LoginActivity.class));
+                        }
+                    });
+
+                } else {
+                    String imgPath = Url.imagePath + response.body().getImage();
+
+                    Picasso.get().load(imgPath).into(imgLogin);
+
+
+                    imgLogin.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Toast.makeText(DashboardActivity.this, "Logged in User: " + response.body().getFullname(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    });
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Toast.makeText(DashboardActivity.this, "Error " + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+//                Picasso.get().load(R.drawable.profile).into(imageProfile);
+            }
+
+        });
 
     }
 }
